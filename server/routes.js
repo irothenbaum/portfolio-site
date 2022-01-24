1b6decc0fee7497c5a028108a4cd0c7b1391c7ff
Rothenbaum <irothenbaum@rxrrealty.com>
2022-01-23 19:06:39 -0500

    Added my wedding site to the portfolio site as a submodule

--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,10 @@ const express = require('express')
 const asyncHandler = require('express-async-handler')
 const router = express.Router()
 const StaticPagesController = require('./controllers/StaticPagesController')
+const path = require("path");
 
 router.get('/', asyncHandler(StaticPagesController.getLanding))
+
+router.use('/mollyandisaacsittinginatree', express.static(path.join(__dirname, '..', 'mollyandisaacsittinginatree')))
 
 module.exports = router
