ec68a1da316e4cd3031396b8da5d74d6ac7b3621
Isaac <irothenbaum@gmail.com>
2022-12-01 09:46:12 -0500

    Added app store link + english app store badge

--- server/constants.js
+++ server/constants.js
@@ -1,5 +1,6 @@
 module.exports = {
   MATH_ATTACK_PLAY_STORE:
-    'https://play.google.com/store/apps/details?id=com.jkfoundit.mathattack&hl=en_US&gl=US',
-  MATH_ATTACK_APP_STORE: '', //TODO
+    'https://play.google.com/store/apps/details?id=com.jkfoundit.mathattack',
+  MATH_ATTACK_APP_STORE:
+    'https://apps.apple.com/us/app/math-attack/id6444708393',
 }
