e8868485f9fcb794e4d48fab9d205131756b55f6
Rothenbaum <irothenbaum@rxrrealty.com>
2024-11-26 09:45:18 -0500

    idk more work on the new pages, but I'm not really feeling this approach anymore... need to think

--- src/scripts/portfolio.js
+++ src/scripts/portfolio.js
@@ -8,7 +8,7 @@
   $(window.document).ready(() => {
     configureNav()
 
-    configureIDELines()
+    // configureIDELines()
   })
 
   // ----------------------------------------------------------------------
